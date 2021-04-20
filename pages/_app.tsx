6a3f8a11d4c20d775278370ad95f382b0c292bdd
rjvelazco <rjvelazco21@gmail.com>
2021-04-20 09:33:32 -0400

    styles.css removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import GlobalStyle from '@styles/Global.styles';
-import '@styles/styles.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
