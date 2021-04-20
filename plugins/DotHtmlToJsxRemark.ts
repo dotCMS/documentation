db7602a6eec0f16fb44dabb129388e8b99d12185
rjvelazco <rjvelazco21@gmail.com>
2021-04-20 14:38:45 -0400

    Removing regular expressions from DotHtmlToJsxRemark plugin

--- plugins/DotHtmlToJsxRemark.ts
+++ plugins/DotHtmlToJsxRemark.ts
@@ -6,19 +6,11 @@ export default function () {
     return function (node): void {
         visit(node, 'jsx', (node) => {
             if (node && node.type === 'jsx') {
-                const inlineTags = new RegExp(/<[img|br|hr][^>]*>/gi);
-                const styleAttr = new RegExp(/(<[^>]+) style=".*?"/gi);
                 const value = node.value as string;
-                const matched = value.match(inlineTags);
-                if (matched) {
-                    let data = value.replace(inlineTags, (match) => {
-                        return transform(match, { plugins: [jsx] });
-                    });
-                    data = data.match(styleAttr) ? data.replace(styleAttr, '$1') : data;
-                    node.value = data;
-                } else if (value.match(styleAttr)) {
-                    node.value = value.replace(styleAttr, '$1');
-                }
+                const data = value.replace(value, (match) => {
+                    return transform(match, { plugins: [jsx] });
+                });
+                node.value = data;
             }
         });
     };
