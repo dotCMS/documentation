569934d49b0dbce87b3f304dacbc1220d408e2b6
rjvelazco <rjvelazco21@gmail.com>
2021-04-21 19:04:32 -0400

    HTML Parser

--- pages/latest/[urlTitle].tsx
+++ pages/latest/[urlTitle].tsx
@@ -29,11 +29,6 @@ import { MDXProvider } from '@mdx-js/react';
 import { MdxRemote } from 'next-mdx-remote/types';
 import { MDXProviderComponentsProp } from '@mdx-js/react';
 
-// Html to ReactJS
-import parse, { attributesToProps } from 'html-react-parser';
-import { HTMLReactParserOptions } from 'html-react-parser';
-import { Element, DataNode } from 'domhandler/lib/node';
-
 interface PageData {
     data: DotcmsDocumentation;
     navDot: DotcmsDocumentation[];
@@ -71,19 +66,19 @@ const urlTitle = ({ data, navDot, source, error }: PageData): JSX.Element => {
             <nav>
                 <DotCollectionNav data={navDot[0]} />
             </nav>
-            <h1>{data.title}</h1>
-            <h2>{data.format}</h2>
-            {data.format === 'html' ? (
-                <div>{parse(data.documentation, htmlToReactOptions)}</div>
-            ) : error ? (
-                <Terminal content={error} />
-            ) : (
-                <div>
-                    <MDXProvider className="wrapper" components={componentsUI}>
-                        {content}
-                    </MDXProvider>
-                </div>
-            )}
+            <div>
+                <h1>{data.title}</h1>
+                <h2>{data.format}</h2>
+                {error ? (
+                    <Terminal content={error} />
+                ) : (
+                    <div>
+                        <MDXProvider className="wrapper" components={componentsUI}>
+                            {content}
+                        </MDXProvider>
+                    </div>
+                )}
+            </div>
         </ContentGrid>
     );
 };
@@ -109,17 +104,13 @@ export async function getStaticProps({
         NAVIGATION_MENU_QUERY
     );
     const { DotcmsDocumentationCollection } = await client.request(FULL_PAGE_QUERY, variables);
-    const format = DotcmsDocumentationCollection[0].format;
     const data = fixHeadingMarkdown(DotcmsDocumentationCollection[0].documentation);
     try {
-        const mdxSource =
-            format === 'markdown'
-                ? await renderToString(data, {
-                      mdxOptions: {
-                          remarkPlugins: [DotHtmlToJsxRemark, remarkId]
-                      }
-                  })
-                : null;
+        const mdxSource = await renderToString(data, {
+            mdxOptions: {
+                remarkPlugins: [DotHtmlToJsxRemark, remarkId]
+            }
+        });
         return {
             props: {
                 data: DotcmsDocumentationCollection[0],
@@ -163,51 +154,5 @@ const fixHeadingMarkdown = (data: string): string => {
     });
     return newData;
 };
-
-const htmlToReactOptions: HTMLReactParserOptions = {
-    htmlparser2: {
-        xmlMode: true
-    },
-    replace: (element: Element): JSX.Element => {
-        if (element.type === 'tag') {
-            if (element.name === 'img') {
-                const props = attributesToProps(element.attribs);
-                return <ImageMarkdown {...props} />;
-            }
-            if (element.name === 'a') {
-                const props = attributesToProps(element.attribs);
-                const children = element.children[0] as DataNode;
-                const propsLink = {
-                    href: props.href,
-                    children: children.data
-                };
-                return <LinkMarkdown {...propsLink} />;
-            }
-            if (element.name === 'span') {
-                const children = element.children[0] as DataNode;
-                return <span>{children.data}</span>;
-            }
-            if (element.name === 'br') {
-                return <br />;
-            }
-        }
-    }
-};
-
-const HtmlToReactJS = `
-<section class="galeria" id="galeria">
-    <img src="/dA/73434262-cd90-4d52-b887-5a2f73f3d476/diagram1" alt="Imagen 1" />
-    <div class="foto">
-        <img src="/dA/73434262-cd90-4d52-b887-5a2f73f3d476/diagram1" alt="Imagen 2">
-    </div>
-    <div class="foto">
-        <img src="/dA/73434262-cd90-4d52-b887-5a2f73f3d476/diagram1" alt="Imagen 3">
-    </div>
-    <div class="foto">
-        <img src="/dA/73434262-cd90-4d52-b887-5a2f73f3d476/diagram1" alt="Imagen 4">
-    </div>
-    <br>
-    <a href="branding-basic-information">Enlace</a>
-</section>`;
 
 export default urlTitle;
