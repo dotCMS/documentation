fec05168e6a4c88450e2a270937262b6fa254673
rjvelazco <rjvelazco21@gmail.com>
2021-05-04 11:05:11 -0400

    Move Layout to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,25 @@ import '@styles/prismjs-theme-dotcms.css';
 
 // Tailwind
 import '@styles/globals.css';
+import { Header } from '../components/header/Header';
+import SideBar from '@components/SideBar';
+import SideNav from '../components/SideNav';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
             <GlobalStyle />
-            <Component {...pageProps} />
+            <div className="flex flex-col min-h-screen">
+                <Header />
+                <div className="flex flex-grow">
+                    <SideBar>
+                        <SideNav data={pageProps.navDot[0]} />
+                    </SideBar>
+                    <div className="container overflow-hidden">
+                        <Component {...pageProps} />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
