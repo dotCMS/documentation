7cd3015b9d4813481b3f51cfcbe94982f2190dc0
rjvelazco <rjvelazco21@gmail.com>
2021-05-06 12:13:40 -0400

    Testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppProps } from 'next/app';
 import GlobalStyle from '@styles/Global.styles';
 import '@styles/prismjs-theme-dotcms.css';
@@ -8,37 +8,36 @@ import styled from 'styled-components';
 import '@styles/globals.css';
 import { Header } from '../components/header/Header';
 import SideBar from '@components/SideBar';
-import SideNav from '../components/SideNav';
+import SideNav from '@components/SideNav';
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: 18rem 1fr;
+    grid-template-columns: max-content 1fr;
     grid-template-rows: max-content 1fr;
     min-height: 100vh;
+    transition: all 4s;
 `;
 const HeaderWrapper = styled.div`
     grid-column: 1/-1;
 `;
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const [hide, updateHide] = useState(false);
+    console.log(pageProps?.navDot);
     return (
         <>
             <GlobalStyle />
-            {pageProps.navDot ? (
-                <Grid>
-                    <HeaderWrapper>
-                        <Header />
-                    </HeaderWrapper>
-                    <SideBar>
-                        <SideNav data={pageProps.navDot[0]} />
-                    </SideBar>
-                    <main className="max-w-7xl justify-self-center mx-8">
-                        <Component {...pageProps} />
-                    </main>
-                </Grid>
-            ) : (
-                <Component {...pageProps} />
-            )}
+            <Grid>
+                <HeaderWrapper>
+                    <Header />
+                </HeaderWrapper>
+                <SideBar hide={hide} updateHide={updateHide}>
+                    <SideNav data={pageProps?.navDot || []} />
+                </SideBar>
+                <main className="max-w-7xl justify-self-center mx-8">
+                    <Component {...pageProps} />
+                </main>
+            </Grid>
         </>
     );
 }
