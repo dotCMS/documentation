38704e82db843e33904e6e0931b4f8ed300a53bf
Freddy Montes <freddymontes@gmail.com>
2021-05-12 17:08:26 -0600

    Fix the wrap text in the nav

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,40 +1,80 @@
 import React, { useState, ReactNode } from 'react';
 import classNames from 'classnames';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    position: absolute;
+    top: 1rem;
+    right: 0;
+`;
 
 const SideBar = ({ children }: { children: ReactNode }): JSX.Element => {
     const [showSidebar, setShowSidebar] = useState(true);
 
-    const containerOnHideClasses = ['md:w-0', 'translate-x-0'];
-    const containerOnShowClasses = ['-translate-x-full'];
+    const contanerBaseClasses = [
+        'bg-white',
+        'border',
+        'border-secondary',
+        'border-t-0',
+        'duration-500',
+        'pt-3',
+        'relative',
+        'relative',
+        'transform',
+        'w-72',
+        'md:duration-500',
+        'md:transition-width',
+        'md:translate-x-0'
+    ];
+    const containerOnHideClasses = [...contanerBaseClasses, 'md:w-0', 'translate-x-0'];
+    const containerOnShowClasses = [...contanerBaseClasses, '-translate-x-full'];
 
+    const buttonBaseClasses = [
+        'bg-gray',
+        'duration-500',
+        'flex',
+        'focus:outline-none',
+        'h-8',
+        'items-center',
+        'md:duration-500',
+        'pl-3',
+        'rounded-l-full',
+        'transform',
+        'transition-transform',
+        'w-8',
+        'z-10'
+    ];
     // Mobile
-    const buttonOnShowMobileClasses = ['rotate-0', 'translate-x-0'];
-    const buttonOnHideMobileClasses = ['rotate-180', 'translate-x-full'];
+    const buttonOnShowMobileClasses = [...buttonBaseClasses, 'rotate-0', 'translate-x-0'];
+    const buttonOnHideMobileClasses = [...buttonBaseClasses, 'rotate-180', 'translate-x-full'];
+
     // Desktop
-    const buttonOnHideDesktopClasses = ['md:rotate-180', 'md:translate-x-full'];
-    const buttonOnShowDesktopClasses = ['md:rotate-0', 'md:translate-x-0'];
+    const buttonOnHideDesktopClasses = [
+        ...buttonBaseClasses,
+        'md:rotate-180',
+        'md:translate-x-full'
+    ];
+    const buttonOnShowDesktopClasses = [...buttonBaseClasses, 'md:rotate-0', 'md:translate-x-0'];
+
     return (
-        <div
-            className={classNames(
-                'bg-white border border-t-0 border-secondary z-50 w-72 pt-3 transform md:translate-x-0 md:transition-width duration-500 md:duration-500 relative',
-                showSidebar ? containerOnShowClasses : containerOnHideClasses
-            )}
-        >
-            <div className="flex justify-end">
-                <button
-                    className={classNames(
-                        'bg-gray bg-transparent flex items-center h-8 pl-3 w-8 z-10 rounded-l-full transform transition-transform duration-500 md:duration-500 focus:outline-none',
-                        showSidebar ? buttonOnShowDesktopClasses : buttonOnHideDesktopClasses,
-                        showSidebar ? buttonOnHideMobileClasses : buttonOnShowMobileClasses
-                    )}
-                    onClick={() => setShowSidebar(!showSidebar)}
-                >
-                    <i className="border border-purple border-t-0 border-r-2 border-b-2 border-l-0 p-1 inline-block transform rotate-135" />
-                </button>
-            </div>
-            <div className="overflow-auto ml-6">
-                <span className="text-sm font-bold text-purple">Overview</span>
-                <nav className="text-gray-200">{children}</nav>
+        <div className={classNames(showSidebar ? containerOnShowClasses : containerOnHideClasses)}>
+            <Button
+                className={classNames(
+                    showSidebar ? buttonOnShowDesktopClasses : buttonOnHideDesktopClasses,
+                    showSidebar ? buttonOnHideMobileClasses : buttonOnShowMobileClasses
+                )}
+                onClick={() => setShowSidebar(!showSidebar)}
+            >
+                <i className="border border-purple border-t-0 border-r-2 border-b-2 border-l-0 p-1 inline-block transform rotate-135" />
+            </Button>
+
+            <div className="md:overflow-hidden">
+                <div className="w-72">
+                    <div className="ml-6 mt-6">
+                        <span className="text-sm font-bold text-purple">Overview</span>
+                        <nav className="text-gray-200">{children}</nav>
+                    </div>
+                </div>
             </div>
         </div>
     );
