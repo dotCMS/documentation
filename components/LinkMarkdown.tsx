8e059e6e79ae96c205339b48ef30a8dce9cbc5c5
rjvelazco <rjvelazco21@gmail.com>
2021-05-25 12:48:40 -0400

    Requested changes

--- components/LinkMarkdown.tsx
+++ components/LinkMarkdown.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Link from 'next/link';
 
 const LinkMarkdown = (props: { href: string; children: string }): JSX.Element => {
-    return !props.href ? (
-        <a>{props.children}</a>
-    ) : props.href.startsWith('#') || props.href.startsWith('http') ? (
-        <a href={props.href}>{props.children}</a>
+    return props.href ? (
+        <>
+            {props.href.startsWith('#') || props.href.startsWith('http') ? (
+                <a href={props.href}>{props.children}</a>
+            ) : (
+                <Link href={props.href}>
+                    <a>{props.children}</a>
+                </Link>
+            )}
+        </>
     ) : (
-        <Link href={props.href}>
-            <a>{props.children}</a>
-        </Link>
+        <a>{props.children}</a>
     );
 };
 
