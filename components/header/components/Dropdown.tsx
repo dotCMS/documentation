38a5e7bc3b200c5d05fe69ef4dd26f8964f87d9f
rjvelazco <rjvelazco21@gmail.com>
2021-05-26 11:53:07 -0400

    Improved styles

--- components/header/components/Dropdown.tsx
+++ components/header/components/Dropdown.tsx
@@ -19,6 +19,11 @@ const Dropdown = (): JSX.Element => {
             id: '1',
             value: 'V8.2.1',
             option: '8.2.1'
+        },
+        {
+            id: '2',
+            value: 'V7.2.1',
+            option: '7.2.1'
         }
     ];
     const selectItem = (item) => {
@@ -28,14 +33,14 @@ const Dropdown = (): JSX.Element => {
     return (
         <div className="cursor-pointer relative">
             <div
-                className="flex items-center content-center justify-between select-none bg-white rounded w-40 h-8 text-xs border border-secondary text-gray px-2"
+                className="flex items-center content-center justify-between select-none bg-white rounded w-40 h-8 text-xs border border-secondary text-gray px-2 pr-43"
                 onClick={() => setHideDropList(!hideDropList)}
             >
                 <span>
                     <span className="font-bold text-sm">Version: </span>
                     {itemSelected.option}
                 </span>
-                <i className="border border-gray border-t-0 border-r-1 border-b-1 border-l-0 p-1 inline-block transform rotate-45" />
+                <i className="relative -top-0.5	border-2 border-gray border-t-0 border-l-0 p-1 inline-block transform rotate-45" />
             </div>
             <div
                 className={classNames(
