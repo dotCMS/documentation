454108de87ebb7d9e45067b680598c3090a7971c
rjvelazco <rjvelazco21@gmail.com>
2021-05-28 09:15:07 -0400

    New simple ImageMarkdown

--- components/ImageMarkdown.tsx
+++ components/ImageMarkdown.tsx
@@ -1,39 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import NextImage from 'next/image';
 
 const ImageMarkdown = (props: { src: string; width?: string; height?: string }): JSX.Element => {
-    const [width, setWidth] = useState(null);
-    const [height, setHeight] = useState(null);
-    const [loaded, setLoaded] = useState(false);
-    if (props.width && props.height) {
-        setWidth(props.width);
-        setHeight(props.height);
-        setLoaded(true);
-    }
-    useEffect(() => {
-        if (!loaded) {
-            const img = new Image();
-            img.src = 'https://dotcms.com' + props.src;
-            img.addEventListener('load', () => {
-                setWidth(img.width);
-                setHeight(img.height);
-                setLoaded(true);
-            });
-        }
-    }, []);
     const myLoader = ({ src }) => src;
     return (
         <>
-            {loaded && (
-                <div className="flex justify-center">
-                    <NextImage
-                        height={height}
-                        layout="intrinsic"
-                        loader={myLoader}
-                        width={width}
-                        {...props}
-                    />
-                </div>
+            {props.width && props.height ? (
+                <NextImage
+                    height={props.height}
+                    layout="intrinsic"
+                    loader={myLoader}
+                    width={props.width}
+                    {...props}
+                />
+            ) : (
+                <img loading="lazy" src={props.src} />
             )}
         </>
     );
