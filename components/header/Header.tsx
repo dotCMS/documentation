65602ca912b9a933675666dba89061cde4502c03
rjvelazco <rjvelazco21@gmail.com>
2021-06-10 15:56:08 -0400

    Header Icons

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -10,20 +10,20 @@ import HamburgerButton from './components/HamburgerButton';
 export const Header = (): JSX.Element => {
     return (
         <header className="md:h-32">
-            <div className="pt-6 px-6 bg-purple h-20 md:h-full flex justify-between content-center md:justify-center md:items-start border-gray-100">
+            <div className="bg-purple border-gray-100 content-center flex h-20 justify-between md:h-full md:items-start md:justify-center pt-6 px-6">
                 <div className="w-2/12">
                     <DotcmsLogo />
                 </div>
-                <div className="hidden px-4 lg:px-0 h-full flex-col justify-center md:flex lg:justify-start w-7/12">
+                <div className="flex-col h-full hidden justify-center lg:justify-start lg:px-0 md:flex px-6 w-7/12">
                     <Search className="rounded" />
                     <nav className="hidden md:inline-block">
                         <HeaderNav />
                     </nav>
                 </div>
-                <div className="hidden h-full md:flex flex-col justify-end items-end w-3/12">
+                <div className="flex-col h-full hidden items-end justify-end md:flex w-3/12">
                     <HeaderSelect />
-                    <Link href="/customer">
-                        <a className="text-white no-underline py-2">Customer Support</a>
+                    <Link href="#">
+                        <a className="no-underline py-2 text-white">Customer Support</a>
                     </Link>
                 </div>
                 <div>
--- components/header/components/HamburgerButton.tsx
+++ components/header/components/HamburgerButton.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 const HamburgerButton = (): JSX.Element => {
     return (
         <button className="flex md:hidden focus:outline-none ">
-            <svg height="30" viewBox="0 0 100 80" width="40">
-                <rect fill="#fff" height="10" width="80" />
-                <rect fill="#fff" height="10" width="80" y="30" />
-                <rect fill="#fff" height="10" width="80" y="60" />
+            <svg
+                fill="none"
+                height="21"
+                width="26"
+                viewBox="0 0 26 21"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <rect fill="white" height="3" rx="1.5" width="26" />
+                <rect fill="white" height="3" rx="1.5" width="26" y="9" />
+                <rect fill="white" height="3" rx="1.5" width="26" y="18" />
             </svg>
         </button>
     );
