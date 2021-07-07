a47f0a8ff6a5c3af553431b4ba9da6b8d97b7719
rjvelazco <rjvelazco21@gmail.com>
2021-07-07 13:01:00 -0400

    Active nav menu on first view

--- components/header/components/HeaderNav.tsx
+++ components/header/components/HeaderNav.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import classNames from 'classnames';
 
 export const HeaderNav = (): JSX.Element => {
     const navTitles = [
-        { title: 'Documentation', id: 'documentation', link: '/' },
-        {
-            title: 'Realease & LTS',
-            id: 'realease',
-            link: '/latest/current-releases'
-        },
-        { title: 'Code Share', id: 'code_share', link: '/codeshare' },
-        { title: 'Forums', id: 'forums', link: 'https://groups.google.com/g/dotcms' },
-        { title: 'Online Training', id: 'online', link: 'https://dotcms.com/courses/' }
+        { title: 'Documentation', link: '/' },
+        { title: 'Realease & LTS', link: '/latest/current-releases' },
+        { title: 'Code Share', link: '/codeshare' },
+        { title: 'Forums', link: 'https://groups.google.com/g/dotcms' },
+        { title: 'Online Training', link: 'https://dotcms.com/courses/' }
     ];
     return (
         <ul className="list-none p-0 m-0">
@@ -24,17 +21,25 @@ export const HeaderNav = (): JSX.Element => {
 const DotNavItem = ({
     navTitles
 }: {
-    navTitles: { title: string; id: string; link: string }[];
+    navTitles: { title: string; link: string }[];
 }): JSX.Element => {
-    const [active, setActive] = useState('documentation');
+    const router = useRouter();
+    const [activeRoute, setActiveRoute] = useState<string>();
+    useEffect(() => {
+        const curretPath = router.asPath;
+        const possibleRoutes =
+            curretPath.includes('/latest/current-releases') || curretPath.includes('/codeshare');
+        possibleRoutes ? setActiveRoute(router.asPath) : setActiveRoute('/');
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     const activeClasses = ['border-b-3', 'border-pink', 'font-bold'];
     return (
         <>
             {navTitles.map((navTitle) => {
-                const isActive = active === navTitle.id;
+                const isActive = activeRoute === navTitle.link;
                 return (
                     <li
-                        key={navTitle.id}
+                        key={navTitle.link}
                         className={classNames(
                             'py-2 mr-8 inline-block no-i',
                             isActive ? activeClasses : null
@@ -43,7 +48,7 @@ const DotNavItem = ({
                         <Link href={navTitle.link}>
                             <a
                                 className="text-white no-underline"
-                                onClick={() => setActive(navTitle.id)}
+                                onClick={() => setActiveRoute(navTitle.link)}
                             >
                                 {navTitle.title}
                             </a>
