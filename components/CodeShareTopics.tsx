b5815f037f551dcede794944fb4d7dd75b71c206
rjvelazco <rjvelazco21@gmail.com>
2021-07-07 13:15:29 -0400

    bold font on the topic of the page - codeshare

--- components/CodeShareTopics.tsx
+++ components/CodeShareTopics.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 // Interfaces
 import { CodeShareTopic } from '@models/CodeShare.model';
 
-export const CodeShareTopics = ({ topics }: { topics: CodeShareTopic[] }): JSX.Element => {
+export const CodeShareTopics = ({
+    topics,
+    tag
+}: {
+    topics: CodeShareTopic[];
+    tag: string;
+}): JSX.Element => {
     return (
         <ul className="list-none">
             {topics.map((topic, index) => (
                 <li key={index}>
                     <Link href={`/codeshare/topic/${topic.link}/1`}>
-                        <a className="no-underline text-blue-500">{topic.title}</a>
+                        <a
+                            className={classNames(
+                                'no-underline text-blue-500',
+                                tag == topic.link && 'font-bold'
+                            )}
+                        >
+                            {topic.title}
+                        </a>
                     </Link>
                 </li>
             ))}
