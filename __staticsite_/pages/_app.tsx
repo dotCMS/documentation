55a08d580c98f5081a7d6de6fba939bb0a9821d8
Rafael <rjvelazco21@gmail.com>
2021-08-30 12:44:59 -0400

    add __staticsite_ folder

---
+++ __staticsite_/pages/_app.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import GlobalStyle from '@styles/Global.styles';
+import '@styles/prismjs-theme-dotcms.css';
+import styled from 'styled-components';
+
+// Tailwind
+import '@styles/globals.css';
+import { Header } from '@components/header/Header';
+import { Footer } from '@components/Footer';
+import { SideBar } from '@components/SideBar';
+import { SideNav } from '@components/SideNav';
+import { useRouter } from 'next/router';
+import { searchBreadCrumb } from '@helpers/searchBreadCrump';
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: ${(props) => (props.codeShare ? '100vw max-content' : '0 100vw 0')};
+    grid-template-rows: max-content 1fr;
+    min-height: 100vh;
+    max-height: 100vh;
+    @media screen and (min-width: 1024px) {
+        grid-template-columns: ${(props) =>
+            props.codeShare ? '1fr max-content' : 'max-content 1fr max-content'};
+    }
+`;
+const HeaderWrapper = styled.div`
+    grid-column: 1/-1;
+`;
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const { pageTitle, navDot, toc } = pageProps;
+    // States
+    const [showSidebar, setShowSidebar] = useState(true);
+    const [showSideToc, setShowSideToc] = useState(false);
+    const [breadCrumb, setBreadcrumb] = useState([]);
+    // Const
+    const title = pageTitle || 'Documentation';
+    const navData = navDot ? navDot[0] : [];
+    const sideNav = navDot;
+    const showTocButton = !!toc?.length;
+    // Router
+    const router = useRouter();
+    const docPage = router.asPath.split('/')[1] || '';
+    useEffect(() => {
+        setBreadcrumb(searchBreadCrumb(navData.dotcmsdocumentationchildren, docPage));
+    }, [docPage]);
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <meta
+                    content="width=device-width, initial-scale=1.0, user-scalable=0"
+                    name="viewport"
+                />
+            </Head>
+            <GlobalStyle />
+            {pageProps.navDot ? (
+                <Grid>
+                    <HeaderWrapper>
+                        <Header
+                            setShowSideToc={setShowSideToc}
+                            setShowSidebar={setShowSidebar}
+                            showSideBar={showSidebar}
+                            showSideButton={sideNav}
+                            showTocButton={showTocButton}
+                        />
+                    </HeaderWrapper>
+                    <SideBar setShowSidebar={setShowSidebar} showSidebar={showSidebar}>
+                        <SideNav breadCrumb={breadCrumb} data={navData} />
+                    </SideBar>
+                    <Component showSideToc={showSideToc} {...pageProps} />
+                </Grid>
+            ) : (
+                <Grid codeShare={true}>
+                    <HeaderWrapper>
+                        <Header
+                            setShowSideToc={setShowSideToc}
+                            setShowSidebar={setShowSidebar}
+                            showSideBar={showSidebar}
+                            showSideButton={sideNav}
+                            showTocButton={showTocButton}
+                        />
+                    </HeaderWrapper>
+                    <div className="flex flex-col overflow-auto">
+                        <Component {...pageProps} />
+                        <Footer />
+                    </div>
+                </Grid>
+            )}
+        </>
+    );
+}
+
+export default MyApp;
---
+++ __staticsite_/pages/search-results/index.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { GetStaticPropsResult } from 'next';
+import { useRouter } from 'next/router';
+
+// Components
+import { Footer } from '@components/Footer';
+import { Loading } from '@components/Loading';
+import { Pagination } from '@components/Pagination';
+import { SearchResult } from '@components/SearchResult';
+
+// Graphql
+import { client } from '@utils/graphql-client';
+import {
+    DOCUMENTATION_SEARCH_COUNT,
+    DOCUMENTATION_SEARCH_QUERY,
+    NAVIGATION_MENU_QUERY
+} from '@graphql/queries';
+
+// Models
+import { Documentation, SearchResultItem } from '@models/Documentation.model';
+
+interface SearchProps {
+    pageTitle?: string;
+    error?: string;
+    navDot?: Documentation[];
+}
+
+interface fetchResultItem {
+    data: SearchResultItem[];
+    totalCount: number;
+}
+
+const Search = (): JSX.Element => {
+    const router = useRouter();
+    // States
+    const [results, setResult] = useState<SearchResultItem[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [totalCount, setTotalCount] = useState<number>();
+    // Variables
+    const search = router.query.search as string;
+    const page = +router.query.page || 1;
+    const totalPages = Math.ceil(totalCount / 10);
+    const baseUrlSearch = `/search-results?search=${search}&page=`;
+    useEffect(() => {
+        if (router.isReady) {
+            setLoading(true);
+            fetchSearchResults(search, page)
+                .then((resp) => resp)
+                .then((resp) => {
+                    setResult(resp.data);
+                    setTotalCount(resp.totalCount);
+                    setLoading(false);
+                });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [router, router.isReady]);
+    return (
+        <div className="overflow-auto flex-col flex flex-grow">
+            <main className="container flex-grow">
+                <h1>
+                    Search: <span className="text-blue-500">{search}</span>
+                </h1>
+                {results.length > 0 ? (
+                    <>
+                        <h3>{totalCount} Results Found</h3>
+                        <div>
+                            {results.map((result, index) => (
+                                <SearchResult key={index} baseUrl={''} data={result} />
+                            ))}
+                        </div>
+                        {totalCount && (
+                            <Pagination
+                                baseUrl={baseUrlSearch}
+                                page={page}
+                                totalPages={totalPages}
+                            />
+                        )}
+                    </>
+                ) : (
+                    <>{loading ? <Loading /> : <h3 className="text-2xl">No results found</h3>}</>
+                )}
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+const fetchSearchResults = async (search: string, page: number): Promise<fetchResultItem> => {
+    const query = `+title:${search}*`;
+    const startFrom = +page <= 1 ? 0 : (+page - 1) * 10;
+    try {
+        // Variables
+        const { DotcmsDocumentationCollection } = await client.request(DOCUMENTATION_SEARCH_QUERY, {
+            search: query,
+            offset: startFrom
+        });
+        const { QueryMetadata } = await client.request(DOCUMENTATION_SEARCH_COUNT, {
+            search: query
+        });
+        return {
+            data: DotcmsDocumentationCollection as SearchResultItem[],
+            totalCount: QueryMetadata[0].totalCount
+        };
+    } catch (e) {
+        return;
+    }
+};
+
+export async function getStaticProps(): Promise<GetStaticPropsResult<SearchProps>> {
+    try {
+        const { DotcmsDocumentationCollection } = await client.request(NAVIGATION_MENU_QUERY);
+        return {
+            props: {
+                pageTitle: 'Search',
+                navDot: DotcmsDocumentationCollection
+            }
+        };
+    } catch (e) {
+        return {
+            props: {
+                pageTitle: 'Search',
+                error: e.message
+            }
+        };
+    }
+}
+
+export default Search;
